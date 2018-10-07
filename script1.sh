b0e6f5cec900bddd225d2d46145c3783a3755739
Arjun N V <arjun.nv04@gmail.com>
2018-10-07 16:36:38 +0530

    my first commit

---
+++ script1.sh
@@ -0,0 +1,12 @@
+#script1.sh
+clear
+echo "Starting Shell script....";
+ls
+read -p "Enter fullname: " fullname;
+git --version;
+cd ../'Git Tutorial';
+git status;
+git add Commands.txt;
+git diff --staged;
+echo "ending of the script";
+//cat script1.sh
